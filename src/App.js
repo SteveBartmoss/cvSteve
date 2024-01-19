a65a0faceefb18aa04d67cc760f26bdce72f03f6
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-18 20:07:08 -0600

    se agrego la estructura del portafolio

--- src/App.js
+++ src/App.js
@@ -4,7 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App App-header">
-      
+      <div>
+        <div>
+          <div>
+            <img src='/Assets/racheBartmoss.jpg' />
+          </div>
+          <div>
+            <h1>Antonio Rivas</h1>
+            <p>Bienvenido a mi portafolio como Desarrollador Trainee</p>
+            <p>Puedes contactar conmigo en mi correo</p>
+            <button>Generar CV</button>
+            <a>Ver CV</a>
+            <h1>lateral</h1>
+          </div>
+
+        </div>
+        <div>
+          <h1>Sobre Mi</h1>
+          <p>
+            Joven con pasión por su profesión que busca mejorar en el desarrollo 
+            web, usuario de sistemas de linux y aplicaciones open source y que busca 
+            busca convertirse en un desarrolador full stack a la vez que aprende nuevas 
+            habilidades y tecnologias.
+          </p>
+          <h1>Actualmente trabajo como codficador externo para Bansi SA</h1>
+        </div>
+        <div>
+          <h1>Porfolio</h1>
+          <p>Poner cards de mis proyectos en steveapp</p>
+        </div>
+        <div>
+          <h1>Educacion</h1>
+        </div>
+        <div>
+          <h1>Skills</h1>
+        </div>
+        <div>
+          <h1>Contacto</h1>
+        </div>
+      </div>
     </div>
   );
 }
