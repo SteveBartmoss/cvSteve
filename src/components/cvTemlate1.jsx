bc977804df075e83bb3bc5134249113e7a28880e
steve <za19012397@zapopan.tecmm.edu.mx>
2025-04-23 08:08:22 -0600

    feat: Se agregaron elementos para el template del portafolio 1

--- src/components/cvTemlate1.jsx
+++ src/components/cvTemlate1.jsx
@@ -1,8 +1,16 @@
-
+import './CvTemplate1.css'
 
 export function CvTemplate1(){
     return(
         <>
+            <div className="App App-header">
+                <div className="div-flex">
+
+                    <div className='div-nav'>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
