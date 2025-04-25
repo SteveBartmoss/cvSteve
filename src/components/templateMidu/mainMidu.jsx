4ee87a615364bdd5574bc1edce2c5fec5c275f99
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-25 12:10:47 -0600

    feat: Se agrego mas estructura semantica para el layout

--- src/components/templateMidu/mainMidu.jsx
+++ src/components/templateMidu/mainMidu.jsx
@@ -1,15 +1,27 @@
 
 
-export function MainMidu(){
-    return(
+export function MainMidu() {
+    return (
         <>
             <div>
-                
-                <h1>Hi, soy Steve <a>Disponible para trabajar</a></h1>
-                <h2>
-                    2 años de experiencia. Ingeniero en Sistema Computacionales y Desarrollador web de Guadalajara, Jalisco. 
-                    Especializado en tecnologias web
-                </h2>
+
+                <main>
+
+                    <section>
+
+                        <h1>Hi, soy Steve <a>Disponible para trabajar</a></h1>
+
+                        <h2>
+                            2 años de experiencia. Ingeniero en Sistema Computacionales y Desarrollador web de Guadalajara, Jalisco.
+                            Especializado en tecnologias web
+                        </h2>
+
+                    </section>
+
+                    <section>
+                        
+                    </section>
+                </main>
 
             </div>
         </>
