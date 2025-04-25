3c53e40e8d9cecba5f4e5a9ee96dff53288dfe43
steve <za19012397@zapopan.tecmm.edu.mx>
2025-04-25 14:50:33 -0600

    feat: Se agregaron elementos para la plantilla del portafolio

--- src/components/templateMidu/mainMidu.jsx
+++ src/components/templateMidu/mainMidu.jsx
@@ -1,20 +1,34 @@
-
+import './mainMiduStyles.css';
 
 export function MainMidu() {
     return (
         <>
-            <div>
+            <div className="main-container">
 
                 <main>
 
-                    <section>
+                    <div>
+                        <img className='img-profile' src='/Assets/racheBartmoss.jpg' alt='profileImg' />
+                        <a>Disponible para trabajar</a>
+                    </div>
 
-                        <h1>Hi, soy Steve <a>Disponible para trabajar</a></h1>
+                    <section className='main-section'>
+
+                        <h1>Hi, soy Steve </h1>
 
                         <h2>
                             2 años de experiencia. Ingeniero en Sistema Computacionales y Desarrollador web de Guadalajara, Jalisco.
                             Especializado en tecnologias web
                         </h2>
+
+                        <ul>
+                            <li>
+                                <a>Contactame</a>
+                            </li>
+                            <li>
+                                <a>Linkedin</a>
+                            </li>
+                        </ul>
 
                     </section>
 
