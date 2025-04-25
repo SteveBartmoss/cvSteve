7094149a7492301c66f65adfcefa1cdd210da20d
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-25 11:29:42 -0600

    feat: Se agrego contenido para la pagina main de la aplicacion

---
+++ src/components/templateMidu/mainMidu.jsx
@@ -0,0 +1,17 @@
+
+
+export function MainMidu(){
+    return(
+        <>
+            <div>
+                
+                <h1>Hi, soy Steve <a>Disponible para trabajar</a></h1>
+                <h2>
+                    2 años de experiencia. Ingeniero en Sistema Computacionales y Desarrollador web de Guadalajara, Jalisco. 
+                    Especializado en tecnologias web
+                </h2>
+
+            </div>
+        </>
+    )
+}
