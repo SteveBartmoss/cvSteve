aa7e7efdc38ed37b1ce40a216db502ed67201245
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-11-08 21:40:22 -0600

    feat: Se implemento una p[lantilla para el portfolio

---
+++ src/components/templateBrittany/templateBrittany.jsx
@@ -0,0 +1,162 @@
+import './mainBrittanyStyles.css';
+
+export function MainBrittany() {
+
+
+    return (
+        <>
+            <div className='div-full'>
+                <div className='div-row'>
+                    <div className='div-section-1'>
+
+
+                        <div className='div-content-section-1'>
+
+                            <h1>Marco Rivas</h1>
+
+                            <p>Fullstack Developer</p>
+
+                            <p>
+                                Desarrollo aplicaciones para el control de las empresas
+                            </p>
+
+                            <div>
+                                <p>Sobre mi</p>
+
+                                <p>Experiencia</p>
+
+                                <p>Proyectos</p>
+                            </div>
+
+                            <div>
+                                <p>GitHub</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div>
+                        <div>
+                            <p>
+                                Desarrollador web fullstack apasionado por crear aplicaciones
+                                para la administracion empresarial o crm personalizados,
+                            </p>
+
+                            <p>
+                                Actualmente soy desarrollador fullstack para la empresa highPower Mexico 
+                                donde cordino a una persona para dar soporte y mantenimiento a las aplicaciones 
+                                de uso interno de la empresa, asi como el desarrollo de nuevos proyectos y actualizciones 
+                                de los proyectos existentes
+                            </p>
+
+                            <p>
+                                Anteriormente tuve la oportunidad de trabajar para grupo carsol en su equipo de desarrollo, 
+                                donde forme parte del proyecto de la creacion del nuevo crm para el uso interno de las agencias 
+                                automotrices del grupo, migraba la logica del negocio de la primera version del crm hacia la version 
+                                dos, tambien colabore en el desarrollo de nuevos modulos para la nueva version del crm y estuve en 
+                                el inicio de la comercializacion del crm para otras agencias automotrices.
+                            </p>
+
+                            <p>
+                                En mi tiempo libre me dedico a desarrollar software de uso libre, actualmente cuento con 
+                                dos paquetes publicados en npm, los cuales actualizo, pruebo y mejoro por mi cuenta. 
+                            </p>
+                        </div>
+
+                        <div>
+
+                            <div>
+                                <div>
+                                    <p>Actualidad</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        Desarrollador web - High Power de Mexico
+                                    </p>
+                                    <p>
+                                        Desarrollador web
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div>
+                                    <p>2024-2025</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        Desarrollador web - Grupo Carsol
+                                    </p>
+                                    <p>
+                                        Desarrollador web
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div>
+                                    <p>2022-2024</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        Desarrollador - Bansi
+                                    </p>
+                                    <p>
+                                        Desarrollador web
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <div>
+                                <div>
+                                    <p>2022-2024</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        CronosJs
+                                    </p>
+                                    <p>
+                                        Libreria de JavaScript para trabajar con fechas
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div>
+                                    <p>2022-2024</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        NeoFetch
+                                    </p>
+                                    <p>
+                                        Wrapper de fetch para trabajar con peticiones http de una forma mas simple
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div>
+                                    <p>2022-2024</p>
+                                </div>
+                                <div>
+                                    <p>
+                                        Jude
+                                    </p>
+                                    <p>
+                                        Libreria Ui para react
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
